1071e9ab98364de17a8b82e0f27475e2181b5677
blu-berry <31569621+blu-berry@users.noreply.github.com>
2017-09-03 17:37:54 +0200

    Update 3
    -Graphical redesign:
                 -Background color is now dark-grey
                 -The sprite is now a blue ring
    -Movement speed has been decreased for smoother feel

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Display")
 clock = pygame.time.Clock()
+gameDisplay.fill((42, 44, 48))
 
 exit = False
 x = 150
@@ -11,7 +12,7 @@ y = 150
 
 
 
-pygame.draw.circle(gameDisplay, (200, 100, 50), (x, y), 15, 0)
+pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
 
 pygame.key.set_repeat(10, 10)
 
@@ -23,28 +24,28 @@ while not exit:
             exit = True
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            x -= 5
-            gameDisplay.fill((0, 0, 0))
+            x -= 3
+            gameDisplay.fill((42, 44, 48))
             if x < 0:
                 x = 0
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            x += 5
-            gameDisplay.fill((0, 0, 0))
+            x += 3
+            gameDisplay.fill((42, 44, 48))
             if x > 800:
                 x = 800
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            y -= 5
-            gameDisplay.fill((0, 0, 0))
+            y -= 3
+            gameDisplay.fill((42, 44, 48))
             if y < 0:
                 y = 0
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            y += 5
-            gameDisplay.fill((0, 0, 0))
+            y += 3
+            gameDisplay.fill((42, 44, 48))
             if y > 600:
                 y = 600
 
         print(x, y)
-        pygame.draw.circle(gameDisplay, (200, 100, 50), (x, y), 15, 0)
+        pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
 
 
 
