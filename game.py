4fad541e242098d747a4be58063c87e4d44a1c01
blu-berry <31569621+blu-berry@users.noreply.github.com>
2017-09-03 16:39:02 +0200

    Update 1
    Added limitations and corrected the controls.

--- game.py
+++ game.py
@@ -15,19 +15,31 @@ pygame.draw.circle(gameDisplay, (200, 200, 200), (x, y), 15, 0)
 
 while not exit:
 
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
 
-
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            y -= 1
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            y += 1
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             x -= 1
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            gameDisplay.fill((0, 0, 0))
+            if x < 0:
+                x = 0
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             x += 1
+            gameDisplay.fill((0, 0, 0))
+            if x > 800:
+                x = 800
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+            y -= 1
+            gameDisplay.fill((0, 0, 0))
+            if y < 0:
+                y = 0
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            y += 1
+            gameDisplay.fill((0, 0, 0))
+            if y > 600:
+                y = 600
 
         print(x, y)
         pygame.draw.circle(gameDisplay, (200, 200, 200), (x, y), 15, 0)
