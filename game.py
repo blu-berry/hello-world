448270d3406a5c121768d4bbd2c6147cd6c9bc9d
blu-berry <31569621+blu-berry@users.noreply.github.com>
2017-09-03 17:13:42 +0200

    Update 2
    -Added continuous movement! 
    -Increased the FPS and movement speed. 
    -Changed the colour to orange-ish.

--- game.py
+++ game.py
@@ -11,7 +11,9 @@ y = 150
 
 
 
-pygame.draw.circle(gameDisplay, (200, 200, 200), (x, y), 15, 0)
+pygame.draw.circle(gameDisplay, (200, 100, 50), (x, y), 15, 0)
+
+pygame.key.set_repeat(10, 10)
 
 while not exit:
 
@@ -21,33 +23,33 @@ while not exit:
             exit = True
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
-            x -= 1
+            x -= 5
             gameDisplay.fill((0, 0, 0))
             if x < 0:
                 x = 0
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
-            x += 1
+            x += 5
             gameDisplay.fill((0, 0, 0))
             if x > 800:
                 x = 800
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
-            y -= 1
+            y -= 5
             gameDisplay.fill((0, 0, 0))
             if y < 0:
                 y = 0
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
-            y += 1
+            y += 5
             gameDisplay.fill((0, 0, 0))
             if y > 600:
                 y = 600
 
         print(x, y)
-        pygame.draw.circle(gameDisplay, (200, 200, 200), (x, y), 15, 0)
+        pygame.draw.circle(gameDisplay, (200, 100, 50), (x, y), 15, 0)
 
 
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(60)
 
 pygame.quit()
 quit()
