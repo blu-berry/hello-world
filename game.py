414e9c90afc49057f7f73cb6d6fee9d8742d7b01
Mihael <31569621+blu-berry@users.noreply.github.com>
2017-09-03 19:40:34 +0200

    Update 4
    - Added a "finish rectangle"
    - It now has an "ending"
    - Added the ability to just change the display resolution and not mess up the collisions
    - Made the map a function so you can edit the map and not worry where is it all being used

--- game.py
+++ game.py
@@ -1,22 +1,38 @@
 import pygame
 
+
+width = 1024
+height = 768
+
+
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+gameDisplay = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Display")
 clock = pygame.time.Clock()
-gameDisplay.fill((42, 44, 48))
+victoryImg = pygame.image.load("victory.jpg")
 
+WIN = False
 exit = False
-x = 150
-y = 150
+x = 512
+y = 758
+
+def map():
+    gameDisplay.fill((42, 44, 48))
+    pygame.draw.rect(gameDisplay, (255, 0, 0), (492, 0, 40, 40), 0)
 
 
+
+map()
+
+def win():
+    gameDisplay.fill((42, 44, 48))
+    gameDisplay.blit(victoryImg, (212, 84))
 
 pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
 
 pygame.key.set_repeat(10, 10)
 
-while not exit:
+while not exit and not WIN:
 
 
     for event in pygame.event.get():
@@ -25,24 +41,29 @@ while not exit:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
             x -= 3
-            gameDisplay.fill((42, 44, 48))
-            if x < 0:
-                x = 0
+            map()
+            if x < 10:
+                x = 10
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             x += 3
-            gameDisplay.fill((42, 44, 48))
-            if x > 800:
-                x = 800
+            map()
+            if x > (width - 10):
+                x = (width - 10)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
             y -= 3
-            gameDisplay.fill((42, 44, 48))
-            if y < 0:
-                y = 0
+            map()
+            if y < 10:
+                y = 10
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             y += 3
-            gameDisplay.fill((42, 44, 48))
-            if y > 600:
-                y = 600
+            map()
+            if y > (height - 10):
+                y = (height - 10)
+        if y < 40:
+            if x < 532 and x > 492:
+                win()
+                WIN = True
+
 
         print(x, y)
         pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
