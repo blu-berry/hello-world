ef745fae03d68fef097540e8a13f0ebdfc85d4c2
Mihael <31569621+blu-berry@users.noreply.github.com>
2017-09-04 15:53:14 +0200

    Update 5
    - Finish is now better
    - Added the capability to add more levels without bugging the game
    - New victory photo

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Display")
 clock = pygame.time.Clock()
-victoryImg = pygame.image.load("victory.jpg")
+victoryImg = pygame.image.load("win.png")
 
 WIN = False
 exit = False
@@ -20,19 +20,21 @@ def map():
     gameDisplay.fill((42, 44, 48))
     pygame.draw.rect(gameDisplay, (255, 0, 0), (492, 0, 40, 40), 0)
 
-
-
 map()
 
 def win():
     gameDisplay.fill((42, 44, 48))
-    gameDisplay.blit(victoryImg, (212, 84))
+    gameDisplay.blit(victoryImg, (362, 134))
+
+maps_list = [map, win]
+
+map_num = 0
 
 pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
 
 pygame.key.set_repeat(10, 10)
 
-while not exit and not WIN:
+while not exit:
 
 
     for event in pygame.event.get():
@@ -41,31 +43,32 @@ while not exit and not WIN:
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
             x -= 3
-            map()
+            maps_list[map_num]()
             if x < 10:
                 x = 10
         if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
             x += 3
-            map()
+            maps_list[map_num]()
             if x > (width - 10):
                 x = (width - 10)
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
             y -= 3
-            map()
+            maps_list[map_num]()
             if y < 10:
                 y = 10
         if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
             y += 3
-            map()
+            maps_list[map_num]()
             if y > (height - 10):
                 y = (height - 10)
         if y < 40:
             if x < 532 and x > 492:
-                win()
-                WIN = True
+                map_num += 1
+                if map_num >= len(maps_list):
+                    map_num = (len(maps_list) - 1)
+                print("You won! Congrats!! :)")
 
 
-        print(x, y)
         pygame.draw.circle(gameDisplay, (40, 119, 252), (x, y), 10, 5)
 
 
